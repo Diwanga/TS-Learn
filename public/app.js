652a470bafb1e2864734e8ee2871ecd24c006241
Diwanga Amasith <diwangaamasith@Diwangas-MacBook-Pro.local>
2022-01-16 14:32:58 +0530

    final2

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@ import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -13,60 +12,25 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values; // TUPLE
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values); // nikan array nm meke typer dan nethi nisa error enawa. TUPLE onemai....
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
-// GENERICS
-// const addUID = (obj: object) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-// const addUID = <T extends object>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docOne = addUID({ name: 'yoshi', age: 40 });
-//let docTwo = addUID('shaun');
-console.log(docOne.name);
-const docThree = {
-    uid: 1,
-    resourceName: 'person',
-    data: { name: 'shaun' }
-};
-const docFour = {
-    uid: 1,
-    resourceName: 'shoppingList',
-    data: ['bread', 'milk']
-};
-console.log(docThree, docFour);
-// ENUMS
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-})(ResourceType || (ResourceType = {}));
-;
-const docOne1 = {
-    uid: 1,
-    resourceType: ResourceType.BOOK,
-    data: { title: 'name of the wind' }
-};
-const docTwo1 = {
-    uid: 10,
-    resourceType: ResourceType.DIRECTOR,
-    data: { title: 'name of the wind' }
-};
-console.log(docOne1); // HERE resource type is print in as number..   0
-console.log(docTwo1); //      4
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+let student;
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
